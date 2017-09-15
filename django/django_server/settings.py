076865f22ac8c05b218fd9b00218c156ef157f3c
Robert Dimitrov <robert_dimitrov@me.com>
2017-09-15 11:03:09 +0300

    Added angular, fixed setup.

--- django/django_server/settings.py
+++ django/django_server/settings.py
@@ -129,4 +129,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.environ['STATIC_ROOT']
 STATIC_URL = '/static/'
--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
-/usr/local/bin/gunicorn docker_django.wsgi:application -w 2 -b :8000
+/usr/local/bin/gunicorn django_server.wsgi:application -w 2 -b :8000
